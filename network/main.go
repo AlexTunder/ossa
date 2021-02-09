1f1751301c13382f120137d811f770dd4a6ce0b4
alexthunder <rakutin2005@mail.ru>
2021-02-09 22:02:51 +0000

    Clean up

--- network/main.go
+++ network/main.go
@@ -1,10 +1,84 @@
 package main
 
-import "fmt"
+//#include "../src/types.h"
+import "C"
 
-var hello int
+/* Connection API */
+//export setServer
+func setServer(host string, port int) int {
+
+	return 0
+}
+
+//export authServer
+func authServer(username string, password string) int {
+
+	return 0
+}
+
+//export useServer
+func useServer(serverID int) int {
+
+	return 0
+}
+
+//export closeServer
+func closeServer(serverID int) int {
+
+	return 0
+}
+
+//export useraddServer
+func useraddServer(name string, pwd string) int {
+
+	return 0
+}
+
+/* Send API */
+/* Messages */
+//export sendMessage
+func sendMessage(mes C.struct_Message) int {
+
+	return 0
+}
+
+//export syncMessages
+func syncMessages(ml C.struct_MessageList) int {
+
+	return 0
+}
+
+//export delMessage
+func delMessage(mes C.struct_Message) int {
+
+	return 0
+}
+
+/* Rolers */
+//export syncRoles
+func syncRoles(roler C.struct_Roler) int {
+
+	return 0
+}
+
+//export syncUsers
+func syncUsers(userList C.struct_UserList) int {
+
+	return 0
+}
+
+//export pullRoles
+func pullRoles(roler C.struct_Roler) int {
+
+	return 0
+}
+
+//export pullUsers
+func pullUsers(userList C.struct_UserList) int {
+
+	return 0
+}
 
 func main() {
-	hello = 1024
-	fmt.Printf("Hello: %v\n", hello)
+
 }
